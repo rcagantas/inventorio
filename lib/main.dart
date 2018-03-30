8f7372194704b33432e6c86f58004dc0bdfaa248
rcagantas <roel.cagantas@gmail.com>
2018-03-30 16:59:39 +0800

    attempt to add barcode reader and camera permissions

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,6 @@ import 'package:flutter/services.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -22,19 +20,9 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
   final String title;
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  @override _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class InventoryItem {
@@ -43,6 +31,21 @@ class InventoryItem {
   var label, expirationDate, barCode;
   InventoryItem({this.label = '', this.expirationDate = '', this.barCode = ''});
 }
+
+class InventoryListItem extends StatelessWidget {
+  final InventoryItem inventoryItem;
+  InventoryListItem(this.inventoryItem): super();
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      leading: new CircleAvatar(),
+      title: new Text(inventoryItem.label),
+      subtitle: new Text(inventoryItem.uuid),
+      trailing: new Text(inventoryItem.expirationDate),
+    );
+  }
+}
+
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<InventoryItem> inventoryItems = new List();
@@ -58,31 +61,17 @@ class _MyHomePageState extends State<MyHomePage> {
     if (!res) await SimplePermissions.requestPermission(Permission.Camera);
   }
 
-  void _addItem(BuildContext context) async {
-    String snack = 'Adding new item';
-    String barCode = '';
-    try {
-      barCode = await BarcodeScanner.scan();
-    } on PlatformException catch(e) {
-      snack = 'Unknown error: $e';
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        snack = 'User did not grant camera access permission';
-      }
-    } on FormatException {
-      snack = 'User returned using back-button';
-    } catch (e) {
-      snack = 'Unknown error: $e';
-    }
-
+  void _addInventoryItem() async {
     setState(() {
       inventoryItems.add(new InventoryItem(
-          label: 'Optional Label',
+          label: 'Optional Label ${inventoryItems.length}',
           expirationDate: 'Expiration Date',
-          barCode: barCode,
       ));
-
-      Scaffold.of(context).showSnackBar(new SnackBar(content: new Text(snack)));
     });
+  }
+
+  void _removeInventoryItem(InventoryItem item) async {
+    setState(() => inventoryItems.remove(item));
   }
 
   @override
@@ -92,17 +81,17 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(title: new Text(widget.title),),
       body: new ListView.builder(
         itemCount: inventoryItems.length,
-        itemBuilder: (BuildContext context, int index) => new ListTile(
-          leading: new CircleAvatar(),
-          title: new Text(inventoryItems[index].label),
-          subtitle: new Text(inventoryItems[index].uuid),
-          trailing: new Text(inventoryItems[index].expirationDate),
-        ),
+        itemBuilder: (BuildContext context, int index) =>
+          new Dismissible(
+            key: new ObjectKey(inventoryItems[index].uuid),
+            child: new InventoryListItem(inventoryItems[index]),
+            onDismissed: (direction) => _removeInventoryItem(inventoryItems[index]),
+          ),
       ),
       floatingActionButton: new Builder(
         builder: (BuildContext context) {
           return new FloatingActionButton(
-            onPressed: () { _addItem(context); },
+            onPressed: _addInventoryItem,
             tooltip: 'Add new inventory item',
             child: new Icon(Icons.add),
           );
