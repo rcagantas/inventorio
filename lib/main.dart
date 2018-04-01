a9181b0b963d23a6d3ab2030c59e35e8d19f2b1c
rcagantas <roel.cagantas@gmail.com>
2018-04-01 22:53:29 +0800

    added new page to handle adding new inventory item

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,12 @@ class MyHomePage extends StatefulWidget {
 class InventoryItem {
   static final uuidGenerator = new Uuid();
   final String uuid = uuidGenerator.v4();
-  var label, expirationDate, barCode, image;
-  InventoryItem({this.label, this.expirationDate, this.barCode, this.image});
+  String label, barCode;
+  DateTime expirationDate;
+  File image;
+  InventoryItem({this.label, this.expirationDate, this.barCode, this.image}) {
+    label = label == null? this.uuid: this.label;
+  }
 }
 
 class InventoryListItem extends StatelessWidget {
@@ -46,10 +50,16 @@ class InventoryListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new ListTile(
-      leading: new CircleAvatar(backgroundImage: new FileImage(inventoryItem.image),),
+      leading: new CircleAvatar(
+        backgroundImage: inventoryItem.image != null?
+          new FileImage(inventoryItem.image): null
+      ),
       title: new Text(inventoryItem.label),
       subtitle: new Text(inventoryItem.uuid),
-      trailing: new Text(inventoryItem.expirationDate),
+      trailing: new Text(
+        inventoryItem.expirationDate == null? '':
+          inventoryItem.expirationDate.toIso8601String().substring(0, 10)
+      ),
     );
   }
 }
@@ -57,27 +67,6 @@ class InventoryListItem extends StatelessWidget {
 class _MyHomePageState extends State<MyHomePage> {
   DateTime lastPickedDate = new DateTime.now();
   final List<InventoryItem> inventoryItems = new List();
-
-  void _addInventoryItem(BuildContext context) async {
-    var expirationDate = await showDatePicker(context: context,
-        initialDate: lastPickedDate,
-        firstDate: lastPickedDate,
-        lastDate: lastPickedDate.add(const Duration(days: 365*5)));
-    if (expirationDate != null) {
-      var image = await ImagePicker.pickImage(source: ImageSource.camera);
-      if (image != null) setState(() {
-        inventoryItems.add(new InventoryItem(
-          label: 'Optional Label ${inventoryItems.length}',
-          expirationDate: expirationDate.toIso8601String().substring(0, 10),
-          image: image,
-        ));
-      });
-    }
-  }
-
-  void _removeInventoryItem(InventoryItem item) async {
-    setState(() => inventoryItems.remove(item));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,17 +79,101 @@ class _MyHomePageState extends State<MyHomePage> {
           new Dismissible(
             key: new ObjectKey(inventoryItems[index].uuid),
             child: new InventoryListItem(inventoryItems[index]),
-            onDismissed: (direction) => _removeInventoryItem(inventoryItems[index]),
+            onDismissed: (direction) {
+              setState(() => inventoryItems.remove(inventoryItems[index]));
+            },
           ),
       ),
       floatingActionButton: new Builder(
         builder: (BuildContext context) {
           return new FloatingActionButton(
-            onPressed: () { _addInventoryItem(context); },
+            onPressed: () async {
+              InventoryItem inventoryItem = await Navigator.push(
+                  context,
+                  new MaterialPageRoute(builder: (context) => new _AddItemPage()));
+              setState(() {
+                if (inventoryItem != null) inventoryItems.add(inventoryItem);
+              });
+            },
             tooltip: 'Add new inventory item',
             child: new Icon(Icons.add),
           );
         }
+      ),
+    );
+  }
+}
+
+class _AddItemPage extends StatefulWidget {
+  @override _AddItemPageState createState() => new _AddItemPageState();
+}
+
+class _AddItemPageState extends State<_AddItemPage> {
+  DateTime lastPickedDate = new DateTime.now();
+  InventoryItem inventoryItem = new InventoryItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Add new item')),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            leading: const Icon(Icons.add_a_photo),
+            title: new FlatButton(
+              onPressed: () async {
+                var image = await ImagePicker.pickImage(source: ImageSource.camera);
+                setState(() { inventoryItem.image = image; });
+              },
+              child: new AspectRatio(
+                aspectRatio: 16.0/9.0,
+                child: new Container(
+                  decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                      fit: BoxFit.fitWidth,
+                      alignment: FractionalOffset.center,
+                      image: inventoryItem.image == null?
+                        new AssetImage('resources/images/milo.jpg'):
+                        new FileImage(inventoryItem.image)
+                    )
+                  ),
+                ),
+              )
+            ),
+          ),
+          new ListTile(
+            leading: const Icon(Icons.today),
+            title: new RaisedButton(
+              child: new Text(
+                inventoryItem.expirationDate != null?
+                inventoryItem.expirationDate.toIso8601String().substring(0, 10):
+                'Expiration Date',
+              ),
+              onPressed: () async {
+                var expirationDate = await showDatePicker(context: context,
+                    initialDate: lastPickedDate,
+                    firstDate: lastPickedDate,
+                    lastDate: lastPickedDate.add(const Duration(days: 365*5)));
+                setState(() {
+                  inventoryItem.expirationDate = expirationDate;
+                });
+              }
+            ),
+          ),
+          new ListTile(
+            leading: const Icon(Icons.label),
+            title: new TextField(
+              decoration: new InputDecoration(hintText: 'Label'),
+              onChanged: (value) {
+                setState(() { inventoryItem.label = value; });
+              },
+            )
+          ),
+        ],
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () { Navigator.of(context).pop(inventoryItem); },
+        child: new Icon(Icons.add_circle_outline),
       ),
     );
   }
