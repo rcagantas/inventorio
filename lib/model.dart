da564cd3ec5889190ef2f990bc564537323c5977
rcagantas <roel.cagantas@gmail.com>
2018-05-16 01:08:34 +0800

    Serialization of user account only.

--- lib/model.dart
+++ lib/model.dart
@@ -14,86 +14,95 @@ part 'model.g.dart';
 
 @JsonSerializable()
 class InventoryItem extends Object with _$InventoryItemSerializerMixin {
-  String uuid, code;
+  String uuid;
+  String code;
   DateTime expiryDate;
   InventoryItem({this.uuid, this.code, this.expiryDate});
   String get expiryDateString => expiryDate?.toIso8601String()?.substring(0, 10) ?? 'No Expiry Date';
-
   factory InventoryItem.fromJson(Map<String, dynamic> json) => _$InventoryItemFromJson(json);
 }
 
 @JsonSerializable()
 class Product extends Object with _$ProductSerializerMixin {
-  String code, name, brand;
+  String code;
+  String name;
+  String brand;
   Product({this.code, this.name, this.brand});
-
   factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
 }
 
 @JsonSerializable()
-class Meta extends Object with _$MetaSerializerMixin {
-  List<String> knownInventories = new List();
-  String currentInventoryMapId, currentProductMapId;
-  Meta(this.currentInventoryMapId, this.currentProductMapId) {
-    knownInventories.add(this.currentInventoryMapId);
-  }
+class InventoryContainer extends Object with _$InventoryContainerSerializerMixin {
+  Map<String, InventoryItem> inventoryItems = new Map();
+  Map<String, Product> products = new Map();
+  String uuid;
+  String name;
+  String createdBy;
+  String createdOn;
+  InventoryContainer({this.uuid, this.name, this.createdBy, this.createdOn});
+  factory InventoryContainer.fromJson(Map<String, dynamic> json) => _$InventoryContainerFromJson(json);
+}
 
-  factory Meta.fromJson(Map<String, dynamic> json) => _$MetaFromJson(json);
+@JsonSerializable()
+class UserAccount extends Object with _$UserAccountSerializerMixin {
+  List<String> knownInventories = new List();
+  String userId;
+  String currentInventoryId;
+  String currentProductId;
+  UserAccount(this.userId, this.currentInventoryId, this.currentProductId) {
+    knownInventories.add(this.currentInventoryId);
+  }
+  factory UserAccount.fromJson(Map<String, dynamic> json) => _$UserAccountFromJson(json);
 }
 
 class AppModel extends Model {
   final Uuid uuidGenerator = new Uuid();
-  final Map<String, InventoryItem> _inventoryItems = new Map();
-  final Map<String, Product> _products = new Map();
 
-  Directory _docDir;
+  InventoryContainer _container = new InventoryContainer();
+  UserAccount _userAccount;
   DateTime _lastSelectedDate = new DateTime.now();
   String _imagePath;
-  Meta _meta;
+  Directory _appDir;
 
   List<InventoryItem> get inventoryItems {
-    List<InventoryItem> toSort = _inventoryItems.values.toList();
+    List<InventoryItem> toSort = _container.inventoryItems.values.toList();
     toSort.sort((item1, item2) => item1.expiryDate.compareTo(item2.expiryDate));
     return toSort;
   }
 
   AppModel() {
-    _init();
-    _ensureSignIn();
     _initAsync();
+    _ensureSignIn();
     print('Item count: ${inventoryItems.length}');
   }
 
-  void _init() async {
-    _docDir = await getApplicationDocumentsDirectory();
-    File metaFile = new File('${_docDir.path}/meta.json');
-
-    if (!metaFile.existsSync()) {
-      _meta = new Meta(uuidGenerator.v4(), uuidGenerator.v4());
-      String metaJson = json.encode(_meta);
-      metaFile.writeAsStringSync(metaJson);
-      _writeInventory();
-      _writeProducts();
-    } else {
-      print('Loading meta');
-      _meta = new Meta.fromJson(json.decode(metaFile.readAsStringSync()));
-
-      Map<String, dynamic> _inventoryJson = json.decode(new File('${_docDir.path}/${_meta.currentInventoryMapId}.json').readAsStringSync());
-      _inventoryJson.forEach((key, itemJson) => _inventoryItems[key] = new InventoryItem.fromJson(itemJson));
-
-      Map<String, dynamic> _productJson = json.decode(new File('${_docDir.path}/${_meta.currentProductMapId}.json').readAsStringSync());
-      _productJson.forEach((key, productJson) => _products[key] = new Product.fromJson(productJson));
-      notifyListeners();
-    }
-    print(_meta.toJson().toString());
-  }
-
   void _writeInventory() {
-    new File('${_docDir.path}/${_meta.currentInventoryMapId}.json').writeAsString(json.encode(_inventoryItems));
+    //new File('${_docDir.path}/${_meta.currentInventoryMapId}.json').writeAsString(json.encode(_inventoryItems));
   }
 
   void _writeProducts() {
-    new File('${_docDir.path}/${_meta.currentProductMapId}.json').writeAsString(json.encode(_products));
+    //new File('${_docDir.path}/${_meta.currentProductMapId}.json').writeAsString(json.encode(_products));
+  }
+
+  void _initAsync() async {
+    _appDir = await getApplicationDocumentsDirectory();
+    Directory imagePickerTmpDir = new Directory(_appDir.parent.path + '/tmp');
+    print('App directory ${_appDir.path}');
+
+    File userAccountFile = new File('${_appDir.path}/userAccount.json');
+    if (userAccountFile.existsSync()) {
+      _userAccount = new UserAccount.fromJson(json.decode(userAccountFile.readAsStringSync()));
+      print('Decoded user account: ${_userAccount.toJson()}');
+    }
+
+    print('Image Picker temp directory ${imagePickerTmpDir.path}');
+    _imagePath = imagePickerTmpDir.path;
+    imagePickerTmpDir.list().forEach((f) {
+      if (f.path.contains('image_picker')) {
+        print('Deleting ${f.path}');
+        f.delete();
+      }
+    });
   }
 
   void _ensureSignIn() async {
@@ -102,18 +111,10 @@ class AppModel extends Model {
     user = user == null ? await googleSignIn.signInSilently() : user;
     user = user == null ? await googleSignIn.signIn() : user;
     print('User: $user');
-  }
-
-  void _initAsync() async {
-    Directory docDir = await getApplicationDocumentsDirectory();
-    Directory imagePickerTmpDir = new Directory(docDir.parent.path + '/tmp');
-    _imagePath = imagePickerTmpDir.path;
-    imagePickerTmpDir.list().forEach((f) {
-      if (f.path.contains('image_picker')) {
-        print('Deleting ${f.path}');
-        f.delete();
-      }
-    });
+    File userAccountFile = new File('${_appDir.path}/userAccount.json');
+    if (!userAccountFile.existsSync()) {
+      userAccountFile.writeAsString(json.encode(new UserAccount(user.id, uuidGenerator.v4(), uuidGenerator.v4())));
+    }
   }
 
   Future<InventoryItem> addItemFlow(BuildContext context) async {
@@ -132,7 +133,7 @@ class AppModel extends Model {
   }
 
   bool isProductIdentified(String code) {
-    return _products.containsKey(code);
+    return _container.products.containsKey(code);
   }
 
   Future<DateTime> getExpiryDate(BuildContext context) async {
@@ -152,26 +153,26 @@ class AppModel extends Model {
   }
 
   void removeItem(String uuid) {
-    _inventoryItems.remove(uuid);
+    _container.inventoryItems.remove(uuid);
     notifyListeners();
     _writeInventory();
   }
 
   void addItem(InventoryItem item) {
-    _inventoryItems[item.uuid] = item;
-    print(json.encode(_inventoryItems));
+    _container.inventoryItems[item.uuid] = item;
+    print(json.encode(_container.inventoryItems));
     notifyListeners();
     _writeInventory();
   }
 
   void addProduct(Product product) {
-    _products[product.code] = product;
+    _container.products[product.code] = product;
     notifyListeners();
     _writeProducts();
   }
 
   Product getAssociatedProduct(InventoryItem item) {
-    return _products[item.code];
+    return _container.products[item.code];
   }
 
   File getImage(String code) {
--- lib/model.g.dart
+++ lib/model.g.dart
@@ -38,19 +38,60 @@ abstract class _$ProductSerializerMixin {
       <String, dynamic>{'code': code, 'name': name, 'brand': brand};
 }
 
-Meta _$MetaFromJson(Map<String, dynamic> json) => new Meta(
-    json['currentInventoryMapId'] as String,
-    json['currentProductMapId'] as String)
+InventoryContainer _$InventoryContainerFromJson(Map<String, dynamic> json) =>
+    new InventoryContainer(
+        uuid: json['uuid'] as String,
+        name: json['name'] as String,
+        createdBy: json['createdBy'] as String,
+        createdOn: json['createdOn'] as String)
+      ..inventoryItems = json['inventoryItems'] == null
+          ? null
+          : new Map<String, InventoryItem>.fromIterables(
+              (json['inventoryItems'] as Map<String, dynamic>).keys,
+              (json['inventoryItems'] as Map).values.map((e) => e == null
+                  ? null
+                  : new InventoryItem.fromJson(e as Map<String, dynamic>)))
+      ..products = json['products'] == null
+          ? null
+          : new Map<String, Product>.fromIterables(
+              (json['products'] as Map<String, dynamic>).keys,
+              (json['products'] as Map).values.map((e) => e == null
+                  ? null
+                  : new Product.fromJson(e as Map<String, dynamic>)));
+
+abstract class _$InventoryContainerSerializerMixin {
+  Map<String, InventoryItem> get inventoryItems;
+  Map<String, Product> get products;
+  String get uuid;
+  String get name;
+  String get createdBy;
+  String get createdOn;
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'inventoryItems': inventoryItems,
+        'products': products,
+        'uuid': uuid,
+        'name': name,
+        'createdBy': createdBy,
+        'createdOn': createdOn
+      };
+}
+
+UserAccount _$UserAccountFromJson(Map<String, dynamic> json) => new UserAccount(
+    json['userId'] as String,
+    json['currentInventoryId'] as String,
+    json['currentProductId'] as String)
   ..knownInventories =
       (json['knownInventories'] as List)?.map((e) => e as String)?.toList();
 
-abstract class _$MetaSerializerMixin {
+abstract class _$UserAccountSerializerMixin {
   List<String> get knownInventories;
-  String get currentInventoryMapId;
-  String get currentProductMapId;
+  String get userId;
+  String get currentInventoryId;
+  String get currentProductId;
   Map<String, dynamic> toJson() => <String, dynamic>{
         'knownInventories': knownInventories,
-        'currentInventoryMapId': currentInventoryMapId,
-        'currentProductMapId': currentProductMapId
+        'userId': userId,
+        'currentInventoryId': currentInventoryId,
+        'currentProductId': currentProductId
       };
 }
