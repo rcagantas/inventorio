b4f104fd0adf9379b24894edb03c9733455dcac6
rcagantas <roel.cagantas@gmail.com>
2018-05-28 20:11:29 +0800

    tab spacing adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -128,20 +128,20 @@ class InventoryItemTile extends StatelessWidget {
                 size: 80.0,
               ):
               new Container(
-                  height: 80.0,
-                  width: 80.0,
-                  decoration: new BoxDecoration(
-                    border: new Border(
-                      top:    BorderSide(width: 1.0, color: Theme.of(context).canvasColor),
-                      bottom: BorderSide(width: 1.0, color: Theme.of(context).canvasColor),
-                      left:   BorderSide(width: 2.0, color: Theme.of(context).canvasColor),
-                      right:  BorderSide(width: 2.0, color: Theme.of(context).canvasColor),
-                    ),
-                    image: new DecorationImage(
-                      image: new FileImage(imageFile),
-                      fit: BoxFit.cover
-                    ),
+                height: 80.0,
+                width: 80.0,
+                decoration: new BoxDecoration(
+                  border: new Border(
+                    top:    BorderSide(width: 1.0, color: Theme.of(context).canvasColor),
+                    bottom: BorderSide(width: 1.0, color: Theme.of(context).canvasColor),
+                    left:   BorderSide(width: 2.0, color: Theme.of(context).canvasColor),
+                    right:  BorderSide(width: 2.0, color: Theme.of(context).canvasColor),
                   ),
+                  image: new DecorationImage(
+                    image: new FileImage(imageFile),
+                    fit: BoxFit.cover
+                  ),
+                ),
                 ),
               new Expanded(
                 flex: 3,
